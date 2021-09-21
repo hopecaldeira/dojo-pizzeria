a29d84cb6378fe104c8f2376e51d5edc881a66c9
Hope Caldeira Smith <hopecynthiana@gmail.com>
2021-09-20 21:33:48 -0500

    first commit

---
+++ pizza.js
@@ -0,0 +1,55 @@
+// PIZZA ONE
+
+function pizzaOven(crust, sauce, cheese, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.sauce = sauce;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var p1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
+console.log(p1);
+
+// PIZZA TWO
+
+function pizzaOven(crust, sauce, cheese, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.sauce = sauce;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var p1 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(p1);
+
+// PIZZA THREE
+
+function pizzaOven(crust, sauce, cheese, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.sauce = sauce;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var p1 = pizzaOven("thin", "marinara", "mozzarella", ["mushrooms", "anchovies", "pineapple"]);
+console.log(p1);
+
+// PIZZA FOUR
+
+function pizzaOven(crust, sauce, cheese, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.sauce = sauce;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var p1 = pizzaOven("deep dish", "alfredo", "parmesean", ["mushrooms", "chicken", "spinach"]);
+console.log(p1);
